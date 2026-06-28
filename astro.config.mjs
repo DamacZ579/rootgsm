029647e92a21fa8e870dfbc3627b627d9bf0f8b2
DamacZ579 <45041963+DamacZ579@users.noreply.github.com>
2026-06-27 22:32:16 -0600

    Remove sitemap integration from astro.config.mjs
    Removed sitemap integration from Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://abogadosxelagsm.com',
   compressHTML: true,
-  integrations: [sitemap()],
 });
